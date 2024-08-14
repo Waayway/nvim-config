ce4d3876dba6741608875580896817845b703249
Waayway <thijs@vanwaaij.com>
2024-08-14 13:24:00 +0200

    Created keybinding for vgit

--- lua/custom/plugins/vgit.lua
+++ lua/custom/plugins/vgit.lua
@@ -3,5 +3,8 @@ return {
 	config = function()
 		local vgit = require("vgit")
 		vgit.setup({})
+		vim.keymap.set("n", "<leader>g", function()
+			vgit.project_diff_preview()
+		end, { desc = "[G]it management" })
 	end,
 }
